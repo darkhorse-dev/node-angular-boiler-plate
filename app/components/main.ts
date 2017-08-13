fb497691c9993e29e5ea253b29bfde166f5857eb
alex0721 <alexandrak0721@gmail.com>
2017-08-13 18:13:10 +0200

    create components

---
+++ app/components/main.ts
@@ -0,0 +1,5 @@
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './testcomponent/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
